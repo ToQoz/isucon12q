57ada96c39e84a520adaed58deba85b70eb186f1
Takatoshi Matsumoto <toqoz403@gmail.com>
2022-07-23 11:59:52 +0900

    update

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,21 +5,21 @@ set -e
 cd /home/isucon/webapp
 git pull
 
-# Copy conf files
+echo "Copy conf files"
 sudo cp conf/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo cp conf/nginx.conf /etc/nginx/nginx.conf
 sudo nginx -t
 sudo cp conf/nginx-isuports.conf /etc/nginx/sites-available/isuports.conf
 
-# Rotate Logs
+echo "Rotate Logs"
 sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d-%H%M%S` || true
 sudo mv /var/log/mysql/slow.log /var/log/mysql/slow.log.`date +%Y%m%d-%H%M%S` || true
 
-# Restart middlewares
+echo "Restart middlewares"
 sudo systemctl restart nginx
 sudo systemctl restart mysql
 
-# Restart app
+echo "Restart app"
 (cd go && go build -o isuports ./cmd/isuports)
 sudo systemctl restart isuports
 
